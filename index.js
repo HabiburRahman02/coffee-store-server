137f1ee8b7050e960904fc759dc77e7f0d82fb5a
Habibur Rahman <habibur88454@gmail.com>
2024-08-17 13:50:53 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('coffee store server is running')
+})
+
+app.listen(port, () => {
+    console.log(`coffee store server is running on port: ${port}`)
+})
